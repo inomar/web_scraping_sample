8b5f8b5514dbb7b5bbad054cdd55529dfd02f470
Makoto Inomata <inomar1486@gmail.com>
2017-07-11 14:47:47 +0900

    add scrape_naver_matome

---
+++ scrape_naver_matome_tech.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://matome.naver.jp/tech'
+
+charset = nil
+
+html = open(url) do |f|
+	charset = f.charset
+	f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('//li[@class="mdTopMTMList01Item"]').each do |node|
+
+p node.css('h3').inner_text
+p node.css('img').attribute('src').value
+p node.css('a').attribute('href').value
+
+end
