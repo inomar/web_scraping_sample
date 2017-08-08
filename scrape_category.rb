f0b4dc0700f1f18ad0ba8231533e10f8a10169db
Makoto Inomata <inomar1486@gmail.com>
2017-08-08 11:26:12 +0900

    add scrate_category

---
+++ scrape_category.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'mechanize'
+
+HOST = 'https://ja.wikipedia.org/'.freeze
+target = 'wiki/Category:%E5%B0%8F%E8%AA%AC%E3%81%AE%E3%82%B8%E3%83%A3%E3%83%B3%E3%83%AB'
+
+mechanize = Mechanize.new
+page = mechanize.get (HOST + target)
+html = Nokogiri::HTML(page.body)
+html = html.xpath("//div[@class='mw-category']/div[@class='mw-category-group']").map do |node|
+  node.search('a').text
+end
+puts html
